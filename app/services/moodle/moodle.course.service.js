a839e87565d83ed91cc758b937190fd6e7a9eb93
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-07-25 18:07:25 -0500

    adding functions to retreive the teacher students

--- app/services/moodle/moodle.course.service.js
+++ app/services/moodle/moodle.course.service.js
@@ -1,3 +1,4 @@
+
 const MoodleServiceBaseClass = require("./moodle.class.service")
 module.exports = class MoodleService extends MoodleServiceBaseClass {
 
@@ -5,26 +6,42 @@ module.exports = class MoodleService extends MoodleServiceBaseClass {
     super.createConnection()
   }
 
-  async getMyStudents(CurrentUser) {
-    const assignments = await getUserAssignments(CurrentUser)
+  async getMyStudents(CurrentUser, opts = { closeOnEnd: true }) {
+    const assignments = await getUserAssignments(CurrentUser, { closeOnEnd: false })
+    const { editingteacher, teacher } = assignments
+
+    if (!editingteacher && !teacher) return []
+
+    const assigmentsAsTeacher = teacher || []
+    const assigmentsAsEditingTeacher = editingteacher || []
+
+
+    const archetype = 'student'
+    const contexts = assigmentsAsEditingTeacher
+      .concat(assigmentsAsTeacher)
+      .map(({ contextid }) => contextid)
+
     const { TABLE_PREFIX } = this
-    
+
     const sql = `
       SELECT
-        ctx.id as 'context',
-      	role.archetype as 'role',
-      	course.id as 'courseid',
-      	course.fullname as 'course'
+        user.id,
+        user.email
       FROM ${TABLE_PREFIX}role_assignments ra
-        INNER JOIN ${TABLE_PREFIX}context ctx ON ctx.id = ra.contextid
-        INNER JOIN ${TABLE_PREFIX}course course ON course.id = ctx.instanceid
-        INNER JOIN ${TABLE_PREFIX}role role ON ra.roleid = role.id
-      WHERE  ra.userid = ? 
+        INNER JOIN ${TABLE_PREFIX}user user ra.userid = user.id
+        INNER JOIN ${TABLE_PREFIX}role role ra.roleid = role.id
+      WHERE 
+        role.archetype in (?) 
+        AND ra.contextid in (?)
     `
+    const preparedValues = [archetype, contexts.join(',')]
+    
+    return this.execute(sql, preparedValues, opts)
+
   }
 
 
-  getUserAssignments(CurrentUser) {
+  async getUserAssignments(CurrentUser, opts = { closeOnEnd: true }) {
     const { TABLE_PREFIX } = this
     const sql = `
       SELECT
@@ -39,10 +56,18 @@ module.exports = class MoodleService extends MoodleServiceBaseClass {
         INNER JOIN ${TABLE_PREFIX}role role ON ra.roleid = role.id
       WHERE  ra.userid = ? 
     `
+
     const { id } = CurrentUser
     const preparedValues = [id]
-    return super.execute(sql, preparedValues, { closeOnEnd: false })
 
+    const results = await super.execute(sql, preparedValues, { closeOnEnd: opts.closeOnEnd })
+    return results
+      .reduce((out, role_assigments) => {
+        const { role } = role_assigments
+        out[role] = out[role] || []
+        out[role].push(role_assigments)
+        return out
+      }, {})
   }
 }
 
