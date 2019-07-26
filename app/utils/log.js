f467ec3c9872ad7eb9c6db148c755012e32f9016
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-07-26 16:05:45 -0500

    log array label

--- app/utils/log.js
+++ app/utils/log.js
@@ -1,11 +1,11 @@
-module.exports = function(){
+module.exports = function () {
 	let types = [];
 	let args = [];
-	for(let arg in arguments){
+	for (let arg in arguments) {
 		let val = arguments[arg];
-		let typeDataObjc = typeof val;
+		let typeDataObjc = Array.isArray(val) ? "array" : typeof val;
 		args.push("[" + typeDataObjc + "]")
-		if(typeDataObjc === "object"){
+		if (typeDataObjc === "object") {
 			let json = JSON.stringify(val, null, 2);
 			arguments[arg] = json === "{}" ? val : json;
 		}
