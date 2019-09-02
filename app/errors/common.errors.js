0c58243336f110315c56b7a4fd8da5f747353341
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-09-02 18:43:20 -0500

    fix the error schema

--- app/errors/common.errors.js
+++ app/errors/common.errors.js
@@ -1,12 +1,11 @@
+const ERROR = require('./constants')
 module.exports = {
 	document_does_not_exist: {
 		http_code: 404,
 		error: {
 			code: -1,
-			error: { resource: 'project.activity', message: "Please set the vpl moodle activity" },
+			error: { resource: 'document', message: "Document not found" },
 			type: ERROR.TYPE.NOT_FOUND
 		}
-
 	}
-
 }
