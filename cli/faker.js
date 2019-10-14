7a82919ca4402e4a69b646d36e740f1d18275953
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-10-14 16:37:01 -0500

    starting to remove cli and use another way to create the fake projects

--- cli/faker.js
+++ cli/faker.js
@@ -1,207 +1,154 @@
 import faker from 'faker'
-import { greenMoneyService } from '../../server/services/green_money/greenMoneyService'
 
-export const PARTNER_TYPES = ['Reseller', 'Platform', 'Platform']
-export const MERCHANT_TYPES = {
-  'E-Commerce': 1,
-  Dispensary: 2,
-}
-export const PASSWORD = 'Abc12345*'
 
-function getSplitRate() {
-  return (Math.random() * 5 + 0.1).toFixed(2)
-}
+export const getProject = () => {
 
-function getECommerceBuyRate() {
-  return (Math.random() * 5 + 0.1).toFixed(2)
-}
-
-function getDispensaryBuyRate() {
-  return (Math.random() * 5 + 0.1).toFixed(2)
-}
-
-function getServiceRate() {
-  return (Math.random() * 5 + 0.1).toFixed(2)
-}
-
-function getRandomDigit(min = 0, max = 9) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-function getNPA() {
-  return [getRandomDigit(2), getRandomDigit(), getRandomDigit()].join('')
-}
-
-function getNXX() {
-  const first = getRandomDigit(2)
-  const second = getRandomDigit()
-  const thirth = second === 1 ? getRandomDigit(2) : getRandomDigit()
-  return [first, second, thirth].join('')
-}
-
-function getXXXX() {
-  return [
-    getRandomDigit(),
-    getRandomDigit(),
-    getRandomDigit(),
-    getRandomDigit(),
-  ].join('')
-}
-
-function getRandomPhoneNumber() {
-  const NPA = getNPA()
-  const NXX = getNXX()
-  const XXXX = getXXXX()
-
-  return [NPA, NXX, XXXX].join('')
-}
-
-const fakerByModel = {
-  consumer: () => {
-    // const twoToNine = ()Math.ceil(Math.random() * 9) + 2
-    return {
-      fake: {
-        email: faker.internet
-          .email()
-          .toLowerCase()
-          .trim(),
-        password: PASSWORD,
-        first_name: faker.name.findName(),
-        last_name: faker.name.lastName(),
-        phone_number: getRandomPhoneNumber(),
+  const project = {
+    "name": "Operadores aritméticos",
+    "description": "Este conjunto de tests evaluará la capacidad del estudiante para usar los diferentes operadores aritméticos",
+    "is_public": true,
+    "activity": 4,
+    "tests": [
+      {
+        "name": "operadores simples",
+        "tags": [
+          "Operadores aritméticos",
+          "Fundamentos de programación"
+        ],
+        "description": "El estudiante deberá crear una clase calculadora con los métodos sumar,restar,dividir y multiplicar. Solo el metodo dividir retornará un double, el resto retornará valores enteros",
+        "objective": "Evaluar los operadores matemáticos básicos",
+        "code": "private Calculadora test \n @Before \n   public void setUp(){ \n  test = new Calculadora(); \n }",
+        "test_cases": [
+          {
+            "name": "Crear clase",
+            "objective": "Evaluar si el estudiante sabe crear la clase",
+            "topic": [
+              "5d39cc1ffb9370034ec5640a",
+              "5d39cc1ffb9370034ec5640c"
+            ],
+            "grade": 2,
+            "successMessage": "Felicidades, sabes usar el operador suma!",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, pero no has conseguido sumar dos numeros.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+sum+numbers+in+java",
+            "code": "Calculadora prueba = new Calculadora()"
+          },
+          {
+            "name": "Sumar simple",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar el operador matemático suma",
+            "grade": 2,
+            "successMessage": "Felicidades, sabes usar el operador suma!",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, pero no has conseguido sumar dos numeros.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+sum+numbers+in+java",
+            "code": "assertEquals(3, test.sumar(1, 2));"
+          },
+          {
+            "name": "Restar simple",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar el operador matemático suma",
+            "grade": 2,
+            "successMessage": "Felicidades, sabes usar el operador resta!",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, pero no has conseguido restar dos numeros.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+diff+numbers+in+java",
+            "code": "assertEquals(1, test.restar(2,1));"
+          },
+          {
+            "name": "Multiplicar simple",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar el operador matemático multiplicar",
+            "grade": 2,
+            "successMessage": "Felicidades, sabes usar el operador multiplicar!",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, pero no has conseguido multiplicar dos numeros.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+multiply+numbers+in+java",
+            "code": "assertEquals(6, test.multiplicar(2,3));"
+          },
+          {
+            "name": "Dividir simple",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar el operador matemático dividir",
+            "grade": 2,
+            "successMessage": "Felicidades, sabes usar el operador dividir!",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, pero no has conseguido dividir dos numeros. Recuerda que tu función debe retornar un double.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+divide+numbers+in+java",
+            "code": "assertEquals(2.5, test.dividir(5,2));"
+          }
+        ]
       },
-    }
-  },
-
-  partner: pType => data => {
-    const { account } = data
-
-    const { email, account_id } = account
-
-    const partner_type =
-      pType && PARTNER_TYPES.includes(pType)
-        ? pType
-        : PARTNER_TYPES[Math.ceil(Math.random() * PARTNER_TYPES.length - 1)]
-
-    const split_rate = getSplitRate(partner_type)
-    const e_commerce_buy_rate = getECommerceBuyRate(partner_type)
-    const dispensary_buy_rate = getDispensaryBuyRate(partner_type)
-    const partner_payload = {
-      account_id,
-      email,
-      organization: faker.company.companyName(),
-      contact_name: `${faker.name.findName()} ${faker.name.lastName()}`,
-      partner_type,
-      e_commerce_buy_rate,
-      split_rate,
-      dispensary_buy_rate,
-    }
-    return { consumer: account, payload: partner_payload }
-  },
-
-  merchant: (mType, partners) => (data, index) => {
-    // get data from last requests
-    const { account: consumer } = data
-
-    const partnerData = partners[index]
-    const { partner: partnerInfo } = partnerData
-    const { id: partner_id, contact_name } = partnerInfo
-
-    const { email } = consumer
-
-    const nMerchantTypes = Object.keys(MERCHANT_TYPES)
-    const ArrayOfMerchantTypes = Object.values(MERCHANT_TYPES)
-
-    const merchant_type = MERCHANT_TYPES[mType]
-      ? MERCHANT_TYPES[mType]
-      : ArrayOfMerchantTypes[
-      Math.ceil(Math.random() * nMerchantTypes.length - 1)
-      ]
-
-    const merchantPayload = {
-      email,
-      merchant_type,
-      rdc_enabled: faker.random.boolean(),
-      icl_enabled: faker.random.boolean(),
-      service_rate: getServiceRate(),
-      status: 1,
-      billing_dba_name: faker.company.companyName(),
-      business_name: faker.company.companyName(),
-      support_email: email,
-      green_client_id: faker.random.word(),
-      green_api_key: faker.random.word(),
-      url: faker.internet.url(),
-      partners: [{ value: partner_id, name: contact_name }],
-      partner_id: partner_id,
-    }
-
-    return { consumer, payload: merchantPayload, partner: partnerData }
-  },
-
-  contactInfo: ({
-    account_id,
-    phone_extension = null,
-    address_1 = faker.address.streetAddress(),
-    address_2 = faker.address.secondaryAddress(),
-    city = faker.address.city(),
-    state = faker.address.stateAbbr(),
-    zip = faker.address.zipCode(),
-    country = 'US',
-  }) => {
-    return {
-      account_id,
-      phone_extension,
-      address_1,
-      address_2,
-      city,
-      state,
-      zip,
-      country,
-    }
-  },
-
-  checkbook: ({
-    account_id,
-    bank_name = faker.company.companyName() + faker.company.companySuffix(),
-    bank_routing_number = faker.phone.phoneNumber('#########'),
-    bank_account_number = faker.phone.phoneNumber('#########')
-  }) => {
-    return {
-      account_id,
-      bank_name,
-      bank_account_number,
-      bank_routing_number
-    }
-  },
-
-  payment: (
-    { merchant, consumer },
-    pStatus = (faker.random.number(4))) => {
-
-    const { merchant_id } = merchant
-    const { account_id, consumer_email, } = merchant
-    const order_amount = faker.random.number(200)
-    const check_id = faker.random.number(10000)
-    const check_number = faker.random.number(10000)
-    const description = 'This is a Fake Payment'
-    const status = greenMoneyService.checkStatus({ Result: '0', VerifyResult: pStatus.toString() })
-
-    return {
-      account_id,
-      merchant_id,
-      order_amount,
-      check_id,
-      check_number,
-      description,
-      status,
-    }
-  },
-
-
-
+      {
+        "name": "operadores encadenados",
+        "tags": [
+          "Operadores aritméticos",
+          "Fundamentos de programación"
+        ],
+        "description": "El estudiante deberá crear una clase calculadora con los métodos sumar,restar,dividir y multiplicar. Solo el metodo dividir retornará un double, el resto retornará valores enteros",
+        "objective": "Evaluar los operadores matemáticos básicos",
+        "code": "private Calculadora test \n @Before \n   public void setUp(){ \n  test = new Calculadora(); \n }",
+        "test_cases": [
+          {
+            "name": "fuerza",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b",
+              "5d39cc1ffb9370034ec56427"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar los operadores matemáticos para obtener la fuerza",
+            "grade": 5,
+            "successMessage": "Felicidades, sabes usar los operadores matematicos para resolver obtener la fuerza",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, no has podido obtener la fuerza.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+calculate+the+force+in+java",
+            "code": "assertEquals(3, test.getFuerza(1, 2));"
+          },
+          {
+            "name": "trabajo",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b",
+              "5d39cc1ffb9370034ec56427"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar los operadores matemáticos para obtener el trabajo",
+            "grade": 5,
+            "successMessage": "Felicidades, sabes usar los operadores matematicos para resolver obtener el trabajo",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, no has podido obtener el trabajo.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+calculate+the+work+in+java",
+            "code": "assertEquals(1, test.getTrabajo(2,1));"
+          },
+          {
+            "name": "presion",
+            "topic": [
+              "5d39cc1ffb9370034ec5640c",
+              "5d39cc1ffb9370034ec5640b",
+              "5d39cc1ffb9370034ec56427"
+            ],
+            "objective": "Evaluar si el estudiante sabe usar los operadores matemáticos para obtener la presion",
+            "grade": 5,
+            "successMessage": "Felicidades, sabes usar los operadores matematicos para resolver obtener la presion",
+            "successMessageLink": "http://www.google.com/search?q=how+to+make+complex+matemathics+operations+in+java",
+            "failureMessage": "Buen intento, no has podido obtener la presion.",
+            "failureMessageLink": "http://www.google.com/search?q=how+to+calculate+the+preasure+in+java",
+            "code": "assertEquals(6, test.getPresion(2,3));"
+          }
+        ]
+      }
+    ]
+  }
 
 }
-
-export const create = modelName => fakerByModel[modelName]
--- cli/index.js
+++ cli/index.js
@@ -2,35 +2,22 @@
 
 import commander from 'commander'
 import { Cli } from './cli.class'
-import { PARTNER_TYPES, MERCHANT_TYPES } from './faker';
 
 const program = new commander.Command()
-program.version('0.0.1')
+program.version('1.0.0')
 program
-  .description('Zodaka CLI helper')
+  .description('Virtual Programming Lab Plus Plus')
 
   .option('-o, --output', 'show output', false)
-
-  .option('-c, --consumer <number>', 'Create consumers', 0)
-
-  .option('-p, --partner <number>', 'Create partners', 0)
-  .option('--partner-type <string>', 'Combined with the parameter -p. The partner type for the partners that will be created. Could be: ' + PARTNER_TYPES.join(','))
-
-  .option('--payment <number>', 'Create payments', 0)
-  .option('--payment-status <number<0-4>>', 'Set the status of the payments that was created before. If the number is out of the valid ranges, the payme', 0)
-
-  .option('-m, --merchant <number>', 'Create merchants', 0)
-  .option('--merchant-type <string>', 'Combined with the parameter -m. The merchant type for the merchants that will be created. Could be: ' + Object.keys(MERCHANT_TYPES).join(','))
-
-
-  .option('-a, --all <number>', 'create consumers, partners and merchants in the same amount', false)
-  .option('--async', 'If true, it will try to make all the possible request async. Can be inestable', false)
-  .option('-s, --session <token>', 'JWT token session. If this is undefined, then we use the admin test credentials to login in the API')
-
+  .option('-p, --project <number>', 'Create project', 0)
+  .option('--teacher <teacher mongo id>', 'Teacher owner')
+  .option('--activity <number>', 'Activity moodle id')
+  .option('--from <string>', 'Project creation date')
+  .option('--to <number>', 'The date of the last submission')
   .parse(process.argv)
 
 const options = program.opts()
-const cli = new Cli('localhost', process.env.PORT, options.async, options.session)
+const cli = new Cli('localhost', process.env.PORT)
 
 Promise.all([
   cli.create(options)
