441f756178a1a1bb6d549b8c73ba7090caaea57a
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-10-23 12:30:22 -0500

    remove commentaries

--- app/routes/project.routes.js
+++ app/routes/project.routes.js
@@ -31,26 +31,12 @@ Router.patch("/:project_id/test/:test_id/case/:id", TestCaseController.update);
 Router.delete("/:project_id/test/:test_id/case/:id", TestCaseController.delete);
 
 
-// from all projects
-// Router.get('/report/', SummaryReportController.getUserReports) // get report of all projects
-// Router.get('/report/timeline', SummaryReportController.getProjectReportTimeline) // get report of all projects
-// Router.get('/report/user/', SummaryReportController.getUserReports) // get report of all users
-// Router.get('/report/user/evolution/', SummaryReportController.getUserEvolution) // get report of students evolution (how the skill level student change along the time)
 Router.get('/report/user/:moodle_student_id/', SummaryReportController.getUserReports) // get report of specific user
 Router.get('/report/user/:moodle_student_id/timeline/', SummaryReportController.getStudentReportTimeline) // get report of students evolution (how the skill level student change along the time)
 
-//  select a project and create a report from it
 Router.get('/:id/report', SummaryReportController.getUserReports) // get report of especific project
 Router.get('/:id/report/timeline', SummaryReportController.getProjectReportTimeline) // get report of especific project
-// Router.get('/:id/report/user/:moodle_student_id', SummaryReportController.getUserReports) // get report of specific user of specific project
-// Router.get('/:id/report/user', SummaryReportController.getUserReports) // get report of all users of specific project
 
-// this routes will be used by the runner, im not following the REST
-// especification because we need add to the vpl ++ jlib
-// the annotations to set the project, test and test case.
-// So we adding a entrypoint to the summary controller
-// Router.get("/test/case/summary/:id?", SummaryController.get);
-// Router.get("/test/:test_case_id/summary/:id?", SummaryController.get);
 Router.post("/test/case/summary/", SummaryController.create);
 
 
