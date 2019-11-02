85958c3573785e9a2b7df9306eda7a0c9327ccdc
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-02 15:37:57 -0500

    remove console.log

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -3,7 +3,6 @@ const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const mongoose = require('mongoose');
 const uri = Config.db.mongo;
 const autoIndex = Config.app.open_development_endpoint
-console.log(autoIndex)
 mongoose.connect(uri, { useNewUrlParser: true, autoIndex });
 mongoose.plugin(beautifyUnique);
 module.exports = mongoose;
