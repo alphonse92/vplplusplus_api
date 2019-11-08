7ba16e5dd34e5ea5c42d911a91e47e07cb1d6555
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-08 18:42:02 -0500

    fix fix fix

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -3,6 +3,6 @@ const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const mongoose = require('mongoose');
 const uri = Config.db.mongo;
 const autoIndex = Config.app.open_development_endpoint
-mongoose.connect(uri, { useNewUrlParser: true, autoIndex });
+mongoose.connect(uri, { useNewUrlParser: true, autoIndex, useUnifiedTopology: true });
 mongoose.plugin(beautifyUnique);
 module.exports = mongoose;
